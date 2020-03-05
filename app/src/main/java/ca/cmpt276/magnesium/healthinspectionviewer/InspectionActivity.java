91c94cf11d7b38ca69a50b5f2161217023c3a4bd
Jsiu <wcsiu@sfu.ca>
2020-03-05 11:56:47 -0800

    Implement the basic layout of Inspection screen

--- app/src/main/java/ca/cmpt276/magnesium/healthinspectionviewer/InspectionActivity.java
+++ app/src/main/java/ca/cmpt276/magnesium/healthinspectionviewer/InspectionActivity.java
@@ -4,9 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
